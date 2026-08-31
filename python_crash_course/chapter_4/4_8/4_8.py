d4bd837e073df260def36afd50f51490e6a17d7b
Kyle Maclachlan <kylem831@gmail.com>
2026-08-30 20:18:15 -0800

    python crash course problem 4.8 finish

--- python_crash_course/chapter_4/4_8/4_8.py
+++ python_crash_course/chapter_4/4_8/4_8.py
@@ -1,7 +1,12 @@
 """
 4-8. Cubes: A number raised to the third power is called a cube. For example,
 the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that
-is, the cube of each intege
+is, the cube of each intege).
 """
-
-
+num = list(range(1,11))
+solution = list()
+for i in num:
+    cube = i**3
+    #print(cube)
+    solution.append(cube)
+print(solution)
